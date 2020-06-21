9f9833b48d516cae2872aeff3932cc130a1b34cc
yess-guns <yessy12384@gmail.com>
2020-06-21 01:49:05 -0500

    Mi primer Hola Mundo con vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HolaMundo></HolaMundo>
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import HolaMundo from './components/HolaMundo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HolaMundo
   }
 }
 </script>
