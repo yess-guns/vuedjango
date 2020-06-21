32dacbfa8278fa3146f19f361937bf8aaa3ea3ea
yess-guns <yessy12384@gmail.com>
2020-06-21 14:34:57 -0500

    Instalación de Bootstrap vue

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <button v-on:click="findAll">Click</button>
+    <div class="box" v-for="e in elements" v-bind:key="e.id">
+      {{ e.title }}
+    </div>
+
   </div>
 </template>
 
 <script>
+//import ListDefault from "../partials/_ListDefault";
 export default {
+  /*components: {
+    ListDefault
+  },*/
+  created() {
+    this.findAll();
+  },
   data() {
     return {
+      elements: []
     };
   },
   methods: {
     findAll: function() {
       fetch("http://localhost:8000/api/element/?format=json")
         .then(res => res.json())
-        .then(res => (console.log(res)));
+        .then(res => (this.elements = res));
     }
   }
 };
 </script>
+
+<style >
+.box {
+  border: 3px solid #ccc;
+  margin: 5px 0 0 0;
+}
+</style>
