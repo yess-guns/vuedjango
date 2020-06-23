b47bac4b6c1552801e6c376de32690dee5695d33
yess-guns <yessy12384@gmail.com>
2020-06-23 03:23:17 -0500

    instalcion de axios y hacer peticiones en la vista de detalle

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,29 +1,60 @@
 <template>
   <div>
-    <b-card :title="element.title">
-    
-      <b-card-text>
-        {{ element.description }}
-      </b-card-text>
+    <b-card>
+      <h1>{{ element.title }}</h1>
+      <div class="p-3">
+        <router-link
+          class="btn btn-danger btn-sm"
+          :to="{ name:'list-category',params:{id: element.id} }"
+        >{{ category.title }}</router-link>
+        <router-link
+          class="ml-2 btn btn-danger btn-sm"
+          :to="{ name:'list-type',params:{id: element.id} }"
+        >{{ type.title }}</router-link>
+        <b-card-text>{{ element.description }}</b-card-text>
+      </div>
     </b-card>
   </div>
 </template>
 
 <script>
+
+const axios = require('axios')
+
 export default {
   created() {
     this.find();
   },
   data() {
     return {
-      element: Object
+      element: Object,
+      category: Object,
+      type: Object
     };
   },
   methods: {
     find: function() {
-      fetch("http://localhost:8000/api/element/1/?format=json")
+      fetch(
+        "http://localhost:8000/api/element/" +
+          this.$route.params.id +
+          "/?format=json"
+      )
         .then(res => res.json())
-        .then(res => (this.element = res));
+        .then(res => {
+          this.element = res;
+          this.findCategory(this.element.id);
+          this.findType(this.element.id);
+        });
+    },
+    findCategory: function(id) {
+      axios.get("http://localhost:8000/api/category/" + id + "/?format=json")
+        //.then(res => res.json())
+        .then(res => (this.category = res.data));
+    },
+    findType: function(id) {
+      axios.get("http://localhost:8000/api/type/" + id + "/?format=json")
+        //.then(res => res.json())
+        .then(res => (this.type = res.data));
     }
   }
 };
